18eca36a0cd80911b8fee7f668f72c1602fd6c6a
jungwoow <yeriel.390@gmail.com>
2026-05-15 01:11:18 +0900

    feat: 모델 수정, admin에 등록

--- django/week02_hw/books/models.py
+++ django/week02_hw/books/models.py
@@ -5,4 +5,4 @@ class Books(models.Model):
     name = models.TextField(verbose_name='제목')
     author = models.TextField(verbose_name='저자')
     rate = models.PositiveIntegerField(verbose_name='평점')
-    
+    text = models.TextField(verbose_name='독후감')

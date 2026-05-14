a8d58bc8bed7b161e84b8a15897d13d03b2fc75f
jungwoow <yeriel.390@gmail.com>
2026-05-14 23:48:31 +0900

    fix: prettier 오류 해결

--- django/week02/posts/views.py
+++ django/week02/posts/views.py
@@ -46,3 +46,7 @@ class class_view(View):
 class class_view2(ListView):
     model = Post
     template_name = 'cbv_view.html'
+
+def home_view(request):
+    context = {"name" : "jungwoo"}
+    return render(request, "home.html", context)

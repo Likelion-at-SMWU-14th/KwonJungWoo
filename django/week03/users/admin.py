1560d758e7b088fc8e070bca72d0b4a4907b7d98
jungwoow <yeriel.390@gmail.com>
2026-05-19 20:51:21 +0900

    feat: users 앱 및 커스텀 User 모델 추가

--- django/week03/users/admin.py
+++ django/week03/users/admin.py
@@ -2,15 +2,6 @@ from django.contrib import admin
 from .models import User
 
 # Register your models here.
-# admin.site.register(User)
-
 @admin.register(User)
-class UserModelAdmiin(admin.ModelAdmin):
-    search_fields = ['nickname']
-    list_display = ['nickname', 'post_count']
-    actions = ['delete']
-
-def delete(self, request, queryset):
-    for item in queryset:
-        item.username = '삭제된 사용자입니다.'
-        item.save()
+class UserAdmin(admin.ModelAdmin):
+    pass
--- django/week03/users/models.py
+++ django/week03/users/models.py
@@ -1,11 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 
 # Create your models here.
+class UserManager(DjangoUserManager):
+    def _create_user(self, username, email, password, phone=None, **extra_fields):
+        if not phone:
+            raise ValueError("전화번호는 필수값입니다.")
+        user = self.model(username=username, email=self.normalize_email(email), phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, username, email=None, password=None, phone=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, phone, **extra_fields)
+    
+    def create_superuser(self, username, email=None, password=None, phone=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(username, email, password, phone, **extra_fields)
 
-class User(models.Model):
-    profile = models.ImageField(verbose_name='프로필사진')
-    nickname = models.CharField(verbose_name='닉네임')
-    username = models.CharField(verbose_name='사용자이름')
-    post_count = models.PositiveIntegerField(verbose_name='게시물개수')
-    following_count = models.PositiveIntegerField(verbose_name='팔로잉수')
-    follower_count = models.PositiveIntegerField(verbose_name='팔로워수')
+class User(AbstractUser):
+    phone = models.CharField(verbose_name='전화번호', max_length=11, unique=True)
+    REQUIRED_FIELDS = ['phone']
+    objects = UserManager()

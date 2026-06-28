4c624de3c93541363ae711d11348cc85240ea0b3
jungwoow <yeriel.390@gmail.com>
2026-06-28 22:00:50 +0900

    feat: 비밀번호 해싱을 위한 create 함수 추가

--- django/week04_hw/accounts/serializers.py
+++ django/week04_hw/accounts/serializers.py
@@ -5,3 +5,11 @@ class UserModelSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user

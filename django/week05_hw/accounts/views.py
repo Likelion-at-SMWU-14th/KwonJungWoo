6ff1a6b3028dc1f99b151366d578547edec3b0db
jungwoow <yeriel.390@gmail.com>
2026-07-02 13:08:09 +0900

    feat: 회원가입 view 구현

--- django/week05_hw/accounts/views.py
+++ django/week05_hw/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from .serializers import UserCreateSerializer, UserLoginSerializer
+
+User = get_user_model()
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserCreateSerializer

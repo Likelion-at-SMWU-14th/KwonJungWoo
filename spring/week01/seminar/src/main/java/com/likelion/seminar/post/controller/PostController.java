9f7442ca5c2e7a9d9a9de27d749e6e7119a7c87c
jungwoow <yeriel.390@gmail.com>
2026-07-21 20:11:39 +0900

    feat: 게시글 수정 기능 구현

--- spring/week01/seminar/src/main/java/com/likelion/seminar/post/controller/PostController.java
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/controller/PostController.java
@@ -33,4 +33,9 @@ public class PostController {
     public PostDTO getPostByRequestParam(@RequestParam("id") int id) {
         return postService.getPostById(id);
     }
+
+    @PutMapping("/{id}")
+    public void updatePost(@PathVariable("id") int id, @RequestBody PostDTO postDTO) {
+        postService.updatePost(id, postDTO);
+    }
 }

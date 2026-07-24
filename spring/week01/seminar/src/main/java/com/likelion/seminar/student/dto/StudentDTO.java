25a0a3e8601344da4ecc18dc6646d348448b3ac3
jungwoow <yeriel.390@gmail.com>
2026-07-24 16:02:38 +0900

    feat: StudentDTO 정의

---
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/student/dto/StudentDTO.java
@@ -0,0 +1,15 @@
+package com.likelion.seminar.student.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDTO {
+    private String studentId;
+
+    private String name;
+
+    private String dateOfBirth;
+}
